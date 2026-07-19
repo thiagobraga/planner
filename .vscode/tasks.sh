27fa970d12361abd6c6ca6d2764d2008158deac8
Thiago Braga <thibraga06@gmail.com>
2026-07-18 22:46:05 -0300

    chore: simplify vscode tasks script

--- .vscode/tasks.sh
+++ .vscode/tasks.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export DOCKER_BUILDKIT=1
-export BUILDKIT_PROGRESS=plain
 docker compose down --timeout=0
 docker compose build
 docker compose up -d
-# sleep 6
-# google-chrome --new-window https://planner.local
